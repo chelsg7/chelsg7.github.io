9e3503428eb87e53721cb6b19d9e52da42e950d9
thorc <chelsgieck@gmail.com>
2018-08-07 19:06:21 -0400

    try

---
+++ src/components/footer/footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Navbar, NavLink } from 'reactstrap';
+import "./foot.css"
+
+class Footer extends React.Component {
+render() {
+    return (
+    <div>
+        <Navbar color="dark" dark id="foot">
+            <Container>
+                <h5 className="text-light" id="footText">
+                    &copy;
+                    &nbsp;
+                    2018 Chelsea Gieck
+                </h5>
+            </Container>
+        </Navbar>
+    </div>
+    );
+}};
+
+export default Footer;
---
+++ src/components/work/work.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
+import './work.css'
+
+class Work extends Component {
+    render() {
+        return (
+        <section id="Portfolio" border>
+            <div>
+                <h1 id="workHead">
+                    Portfolio
+                </h1>
+            </div>
+            <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                                <h4>
+                                    Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                                </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                                <h4>
+                                    Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake. 
+                                </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake. 
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card id="card">
+                            <CardHeader>
+                                title
+                            </CardHeader>
+                            <CardBody>
+                            <h4>
+                                Carrot cake caramels cheesecake cupcake gingerbread danish fruitcake cake.
+                            </h4>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+            </div>
+        </section>
+    );
+}}
+
+export default Work;
